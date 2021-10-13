19cc76f0084adad1db0812090568c825e850ceaa
JimA <jm.allan@ntlworld.com>
2021-10-13 16:53:17 +0100

    Another commit - Changed to SECRET_KEY again to avoid environment variable.  Need to get wesite working- Last Chance before moving on.

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ from flask import abort
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_Key'] = os.environ.get('SECRET_Key')
-#app.config['SECRET_Key'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+#app.config['SECRET_KEY] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -284,4 +284,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
